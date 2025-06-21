0d1a859a1cd609e913228b3a125485a30e1285a0
sandipondas <sandipondas25@gmail.com>
2025-06-21 18:17:59 +0600

    add string related file in doctor

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from . import models
 
 class DoctorSerializer(serializers.ModelSerializer):
-    # user = serializers.StringRelatedField(many=False)
+    user = serializers.StringRelatedField(many=False)
     # specialization = serializers.StringRelatedField(many=True)
     # designation = serializers.StringRelatedField(many=True)
     # available_time = serializers.StringRelatedField(many=True)
