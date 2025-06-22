6c27ec5fe48c42b45ac67affd05e8d27e91b9230
sandipondas <sandipondas25@gmail.com>
2025-06-22 16:38:31 +0600

    edit in appointment serili..

--- appointment/serializers.py
+++ appointment/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from .import models
+from doctor.models import AvailableTime
+
+class AvailableTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AvailableTime
+        fields = ['name']  # Adjust according to your model field
 
 class AppointmentSerializer(serializers.ModelSerializer):
-    time = serializers.StringRelatedField(many =False)
+    # time = serializers.StringRelatedField(many =False)
     # doctor = serializers.StringRelatedField(many = False)
     # patient = serializers.StringRelatedField(many = False)
+    time_detail = AvailableTimeSerializer(source='time', read_only=True) 
     class Meta:
         model = models.Appointment
         fields = '__all__'
